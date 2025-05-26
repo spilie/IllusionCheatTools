2e6a34d315eaea0b54835d89c656fb29a82e35ee
spilie <ccy9366@gmail.com>
2025-05-27 07:14:11 +0800

    Ver1.0.2

--- AI_NoAIDoor/NoAIDoorPlugin.cs
+++ AI_NoAIDoor/NoAIDoorPlugin.cs
@@ -1,15 +1,8 @@
-﻿using AIChara;
-using AIProject;
+﻿using AIProject;
+using AIProject.Player;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
-using KKAPI.Chara;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AI_NoAIDoor
 {
@@ -17,33 +10,15 @@ namespace AI_NoAIDoor
     public class NoAIDoorPlugin : BaseUnityPlugin
     {
         private static ManualLogSource _logger;
-
         public const string GUID = "hakin.NoAIDoor";
         public const string Version = "1.0";
+
         private void Awake()
         {
             _logger = base.Logger;
-
-             var h = Harmony.CreateAndPatchAll(typeof(NoAIDoorPlugin), GUID);
-            Logger.LogInfo("NoAIDoorPlugin initialized.");
-        }
-
-
-        private static class Patches
-        {
-            [HarmonyPrefix]
-            [HarmonyPatch(typeof(Actor), "SendEvent")]
-            static bool PreventAIDoorOpen(ref Actor __instance)
-            {
-                if (__instance.EventKey == EventType.DoorOpen)
-                {
-                    _logger.LogInfo($"Prevented AI {__instance.name} from opening door.");
-                    __instance.EventKey = EventType.DoorClose;
-                    return false; // 阻止原始事件發送
-                }
-
-                return true; // 允許其他事件通過
-            }
+            NoDoorHook.SetLogger(_logger);
+            Harmony.CreateAndPatchAll(typeof(NoDoorHook), GUID);
+            _logger.LogInfo("NoAIDoorPlugin initialized.");
         }
     }
 }
---
+++ AI_NoAIDoor/NoDoorHook.cs
@@ -0,0 +1,23 @@
+﻿using AIProject;
+using BepInEx.Logging;
+using HarmonyLib;
+
+public static class NoDoorHook
+{
+    private static ManualLogSource _logger;
+
+    public static void SetLogger(ManualLogSource logger) => _logger = logger;
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Actor), nameof(Actor.EventKey))]
+    public static bool PreventAIDoorOpen(Actor __instance, EventType type)
+    {
+        if (type == EventType.DoorOpen)
+        {
+            _logger?.LogInfo($"[NoAIDoor] Blocked {__instance.CharaName} from opening door.");
+            return false;
+        }
+
+        return true;
+    }
+}
